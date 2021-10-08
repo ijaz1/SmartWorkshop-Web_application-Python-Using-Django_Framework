e1cb4961c7d3f7cc1fbe50e10793df2e69c27bf3
ijaz1 <muhammedijazkari168@gmail.com>
2021-10-08 11:11:33 +0530

    second update

--- p1/settings.py
+++ p1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-olo=mem=6bp20exhvf&-+8qsduqn&=^6@b+%b6^sv3)!aym@fv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
