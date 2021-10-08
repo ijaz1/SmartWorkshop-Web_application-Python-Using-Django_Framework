2bd0d71b2b8f860eb44fab6768ff11bf4ea24eca
ijaz1 <muhammedijazkari168@gmail.com>
2021-10-08 10:39:15 +0530

    first update

--- app1/views.py
+++ app1/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
-def hfun(request):
-    return HttpResponse('wow!')
     
+def adlogfun(request):
+    return render(request,'index.html')
